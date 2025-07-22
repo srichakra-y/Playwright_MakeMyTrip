363943b086f541df3250a38401be36d359c2cefd
Srichakra Y <srichakra543@gmail.com>
2025-07-22 16:23:27 +0530

    Removed Tests

--- tests/MakeMyTrip.spec.js
+++ tests/MakeMyTrip.spec.js
@@ -1,13 +1,10 @@
 import { test, expect, chromium } from "@playwright/test"
 import { HomePage } from "../Pages/HomePage"
-
 import { CabsPage } from "../Pages/cabsPage"
 import { SUVCarPage } from "../Pages/SUVCarPage";
 import { GiftCardPage } from "../Pages/GiftCardPage";
 import { HotelsPage } from "../Pages/HotelsPage";
 import process from "../Inputs/GiftCardInputValues.json";
-
-
 import { SaudiPage } from "../Pages/SaudiPage";
 
 
@@ -16,43 +13,6 @@ test.beforeEach("Creating Page", async() => {
     browser = await chromium.launch({ args:['--start-maximized']});
     context = await browser.newContext({ viewport: null, deviceScaleFactor: undefined });
     page = await context.newPage();
-
-})
-
-test('Booking cabs', async() => {
-
-    const homepage = new HomePage(page, context);
-    await homepage.goToHomePage();
-    await homepage.clickOnCabsMenu();
-
-    const cabspage = new CabsPage(page);
-    await cabspage.fromCitySelection();
-    await cabspage.toCitySelection();
-    await cabspage.dateSelection();
-    await cabspage.pickupTimeSelection();
-    await cabspage.searchButtonSelection();
-    const suvcarpage = new SUVCarPage(page);
-    await suvcarpage.suvCarSelection();
-    await suvcarpage.carNamePrice();
-})
-
-test('Checking the giftcards', async() => {
-    const homepage = new HomePage(page, context);
-    await homepage.goToHomePage();
-    const newPage = await homepage.clickOnGiftCardMenu();
-    const giftcardpage = new GiftCardPage(newPage);
-    await giftcardpage.giftCardSelection();
-    await giftcardpage.recipientDetailsFilling(process.recipientName,process.recipientMobileNumber,process.recipientEmail);
-    await giftcardpage.senderDetailsFilling(process.senderName,process.senderMobileNumber,process.senderEmail);
-    await giftcardpage.errorMessageCapturing();
-})
-
-test('Checking adults selection in Hotels', async() =>{
-    const homepage = new HomePage(page, context);
-    await homepage.goToHomePage();
-    await homepage.clickOnHotelsMenu();
-    const hotelspage = new HotelsPage(page);
-    await hotelspage.guestsSelection();
 })
 
 test.describe('Basic Functionality', async() => {
@@ -81,13 +41,9 @@ test.describe('Basic Functionality', async() => {
         const saudipage = new SaudiPage(newPage1);
         await saudipage.validatingIconsSaudi();
     })
-    test.only("sample",async()=>{
-        console.log("raju created test");
-    })
 })
 
 test.describe('Cabs Test Cases', async() => {
-
     test("LOGO Verification", async()=>{
         const homepage = new HomePage(page, context);
         await homepage.goToHomePage();
@@ -141,7 +97,6 @@ test.describe('Cabs Test Cases', async() => {
 })
 
 test.describe('Giftcard Test Cases', async() => {
-
     test('Fetching festival giftcards', async() => {
         const homepage = new HomePage(page, context);
         await homepage.goToHomePage();
@@ -183,7 +138,6 @@ test.describe('Giftcard Test Cases', async() => {
 })
 
 test.describe("hotel test cases",async()=>{
-
     test("Check the hotel link is working",async()=>{
         const homepage = new HomePage(page, context);
         await homepage.goToHomePage();
